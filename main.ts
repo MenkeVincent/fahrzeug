7b009cea721572bc1d1ba08c26ad32e575bb6711
MenkeVincent <97405205+MenkeVincent@users.noreply.github.com>
2022-01-10 20:11:58 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,51 +7,37 @@ enum RadioMessage {
     Abbrechen = 21951,
     Kompass = 23398
 }
-radio.onReceivedMessage(RadioMessage.Abbrechen, function () {
-    Folgen_oder_nicht_oder_Abbrechen = 3
-})
-radio.onReceivedMessage(RadioMessage.Follow, function () {
-    Folgen_oder_nicht_oder_Abbrechen = 1
-})
-radio.onReceivedMessage(RadioMessage.StopFollowing, function () {
-    Folgen_oder_nicht_oder_Abbrechen = 2
+radio.onReceivedNumber(function (receivedNumber) {
+    Wert = receivedNumber
 })
 radio.onReceivedValue(function (name, value) {
-    if (name == "compass") {
-        KompasswertController = value
-    }
-    if (name == "Neigung") {
+    if (name == "nicken") {
         NeigungswertController = value
     }
 })
-let abweichungzugering = 0
 let NeigungswertController = 0
-let KompasswertController = 0
-let Folgen_oder_nicht_oder_Abbrechen = 0
+let Wert = 0
 radio.setGroup(69)
 radio.setTransmitPower(20)
-Folgen_oder_nicht_oder_Abbrechen = 2
+let Folgen_oder_nicht_oder_Abbrechen = 2
 basic.forever(function () {
-    while (Folgen_oder_nicht_oder_Abbrechen == 1) {
-        if (input.compassHeading() - KompasswertController < 5) {
-            abweichungzugering = 1
+    if (Wert <= -1) {
+        if (Wert < -45) {
+            motors.dualMotorPower(Motor.A, 1000)
+            motors.dualMotorPower(Motor.B, 40)
         } else {
-            abweichungzugering = 0
+            motors.dualMotorPower(Motor.B, 50)
         }
-        if (360 - input.compassHeading() < 180 && abweichungzugering == 0) {
-            while (KompasswertController - input.compassHeading() >= 5) {
-                motors.dualMotorPower(Motor.A, -6)
-                motors.dualMotorPower(Motor.B, 6)
-            }
-        } else if (360 - input.compassHeading() > 180 && abweichungzugering == 0) {
-            while (KompasswertController - input.compassHeading() >= 5) {
-                motors.dualMotorPower(Motor.B, -6)
-                motors.dualMotorPower(Motor.A, 6)
-            }
+    } else if (Wert >= 1) {
+        if (Wert >= 45) {
+            motors.dualMotorPower(Motor.B, 1000)
+            motors.dualMotorPower(Motor.A, 45)
         } else {
-            if (Folgen_oder_nicht_oder_Abbrechen == 1 && abweichungzugering == 1) {
-                motors.dualMotorPower(Motor.AB, NeigungswertController)
-            }
+            motors.dualMotorPower(Motor.A, 50)
         }
+    } else {
+        motors.dualMotorPower(Motor.AB, NeigungswertController)
+        basic.clearScreen()
+        radio.sendNumber(input.temperature())
     }
 })
